1939a2f47dbaa605cf89d9d8a5bbb817c9576a7e
Valmir Junior <jnrplay@hotmail.com>
2023-05-27 20:06:13 -0300

    refactor: static styles config

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,18 +1,18 @@
-import { HttpResponse, HttpRequest } from "../protocols/http";
+import { type HttpResponse, type HttpRequest } from '../protocols/http';
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error("Missing param: name"),
+        body: new Error('Missing param: name')
       };
     }
 
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error("Missing param: email"),
+        body: new Error('Missing param: email')
       };
     }
   }
