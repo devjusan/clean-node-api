ab9b5d9b8b3fc101e497a93fe3396fb1c7938bc6
Valmir Junior <jnrplay@hotmail.com>
2023-05-29 22:11:08 -0300

    chore: add jest configs and scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,6 @@
+/* eslint-disable semi */
+const config = require('./jest.config');
+
+config.testMatch = ['**/*.test.ts'];
+
+module.exports = config;
---
+++ jest-unit-config.js
@@ -0,0 +1,6 @@
+/* eslint-disable semi */
+const config = require('./jest.config');
+
+config.testMatch = ['**/*.spec.ts'];
+
+module.exports = config;
