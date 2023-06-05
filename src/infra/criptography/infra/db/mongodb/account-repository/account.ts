052e30e41e1af9d4cca2c9e5fe592bfa8744f5c8
Valmir Junior <jnrplay@hotmail.com>
2023-06-05 20:03:03 -0300

    feat: ensure accountmongorepository returns account on success

---
+++ src/infra/criptography/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,16 @@
+import { type AddAccountRepository } from '../../../../../../data/protocols/add-account-repository';
+import { type AccountModel } from '../../../../../../domain/models/account';
+import { type AddAccountModel } from '../../../../../../domain/usecases/add-account';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const copiedAccountData = { ...accountData };
+    const accountCollection = MongoHelper.getCollection('accounts');
+    const {
+      insertedId: { id }
+    } = await accountCollection.insertOne(copiedAccountData);
+
+    return Object.assign({}, accountData, { id: id.toString() });
+  }
+}
