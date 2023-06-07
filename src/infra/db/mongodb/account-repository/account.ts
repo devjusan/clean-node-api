775a805f2b8ca39e0a186faa4f9fb8e99741eba4
Valmir Junior <jnrplay@hotmail.com>
2023-06-07 19:49:43 -0300

    feat: connect to mongodb before app listen

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,6 +1,7 @@
-import { type AddAccountRepository } from '../../../../../../data/protocols/add-account-repository';
-import { type AccountModel } from '../../../../../../domain/models/account';
-import { type AddAccountModel } from '../../../../../../domain/usecases/add-account';
+
+import { type AddAccountRepository } from '../../../../data/protocols/add-account-repository';
+import { type AccountModel } from '../../../../domain/models/account';
+import { type AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 
 export class AccountMongoRepository implements AddAccountRepository {
